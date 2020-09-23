ff6b5085642cb177827024e0058d7a998bf4d754
Damo Hart <damohart@gmail.com>
2020-09-23 06:33:17 +0000

    Lab 7

---
+++ config.py
@@ -0,0 +1,6 @@
+ecs_test_drive = {
+    'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
+    'ecs_access_key_id' : '132276173696851222@ecstestdrive.emc.com',
+    'ecs_secret_key' : '0KbZxM5WcjgILEr/3F0tWIxJkMqahXbejMx1WBhp',
+    'ecs_bucket_name' : 'photo-album'
+}
