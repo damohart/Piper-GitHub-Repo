f8552596023c7995b3681ba6a7dd37e42495482b
Damo Hart <damohart@gmail.com>
2020-09-23 06:54:59 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ ecs_test_drive = {
     'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
     'ecs_access_key_id' : '132276173696851222@ecstestdrive.emc.com',
     'ecs_secret_key' : '0KbZxM5WcjgILEr/3F0tWIxJkMqahXbejMx1WBhp',
-    'ecs_bucket_name' : 'photo-album'
+    'ecs_bucket_name' : 'damos-photo-album'
 }
